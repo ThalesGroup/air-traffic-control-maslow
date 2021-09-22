9d1cad2d4e01d1f859cddf0d7971856010b765f0
AreskiH <areski.hadjaz@thalesdigital.io>
2021-09-22 15:05:17 +0200

    Source Code THALES Disclaimer added

--- client/src/ext/routing/AsyncLoader.tsx
+++ client/src/ext/routing/AsyncLoader.tsx
@@ -1,16 +1,28 @@
-import * as React from 'react'
-
-/**
- * 
- * Loads a component asynchronously, interpreted by webpack which splits the file
- * 
- * export default class Login extends React.Component<any, any> {
- * 
- * }
- * 
- * 
- * const Login = () => <AsyncLoader comp={import('./page/Login')} />
+/*
+ *  ----------------------------------------------------------------------------
+ *
+ *  Copyright (c) 2021 - THALES LAS/AMS
+ *
+ *  -----------------------------------------------------------------------------
+ *  THALES MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF
+ *  THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ *  TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ *  PARTICULAR PURPOSE, OR NON-INFRINGEMENT. THALES SHALL NOT BE
+ *  LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING,
+ *  MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
+ *
+ *  THIS SOFTWARE IS NOT DESIGNED OR INTENDED FOR USE OR RESALE AS ON-LINE
+ *  CONTROL EQUIPMENT IN HAZARDOUS ENVIRONMENTS REQUIRING FAIL-SAFE
+ *  PERFORMANCE, IN WHICH THE FAILURE OF THE
+ *  SOFTWARE COULD LEAD DIRECTLY TO DEATH, PERSONAL INJURY, OR SEVERE
+ *  PHYSICAL OR ENVIRONMENTAL DAMAGE ("HIGH RISK ACTIVITIES"). THALES 
+ *  SPECIFICALLY DISCLAIMS ANY EXPRESS OR IMPLIED WARRANTY OF FITNESS FOR
+ *  HIGH RISK ACTIVITIES.
+ *
+ *  -----------------------------------------------------------------------------
  */
+
+ import * as React from 'react'
 
 export class AsyncLoader extends React.Component<Props, any> {
     
